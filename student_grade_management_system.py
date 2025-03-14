27d9e4e0109631210f63e23c9ad9ebb67bc63dd2
akbhardvaj47 <amitbhardvaj830@gmail.com>
2025-03-14 19:44:26 +0530

    modify some code

--- student_grade_management_system.py
+++ student_grade_management_system.py
@@ -1,10 +1,12 @@
-all_students={'Amit':800}
+# Initialize an empty dictionary to add student name and grade
+all_students={}
 def add_student(name,grade):
     all_students[name]=grade
     print(all_students)
     print(f'Student {name} is added successfully')
 # add_student()
 
+# Function to update the name and grade of user in dictionary
 def update_student(name):
     if name in all_students:
         old_student=all_students.pop(name)
@@ -14,20 +16,24 @@ def update_student(name):
         print(f'{name} is updated by {new_student} is successfully...')
 # update_student() 
 
+# Function to delete a student details from dictionary
 def delete_student(name):
     del all_students[name]
     print(f'Student {name} is deleted successfully')
 # delete_student() 
 
+# Function to display all the students name with their grade
 def display_students():
     if all_students:
         for name,grade in all_students.items():
             print(f'{name} {grade}')
 # display_students()
 
+# Function to exit from the app
 def exit_app():
     print(f'You are exiting, Goodbye!')
     
+# Main function    
 def body():
     while True:
         print('1. Add a student')
@@ -35,6 +41,7 @@ def body():
         print('3. Delete a student')
         print('4. Display all students')
         print('5. Exit / Stop')
+        # Choice for user to confirm that what user wants to do
         choice=int(input('Enter your choice : '))
         if choice==1:
             name=input('Enter name :')
